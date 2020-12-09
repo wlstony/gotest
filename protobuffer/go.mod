ddd9c725659761ec902d20e06ac39192e8386a49
wuls <wuls@2345.com>
2020-12-09 19:19:00 +0800

    微服务测试

---
+++ protobuffer/go.mod
@@ -0,0 +1,8 @@
+module protobuffer
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.3
+	google.golang.org/protobuf v1.23.0
+)
---
+++ protobuffer/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"log"
+)
+
+func main() {
+	test := &Student {
+		Name:   "geektutu",
+		Male:   true,
+		Scores: []int32{98, 85, 88},
+	}
+	data, err := proto.Marshal(test)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	newTest := &Student{}
+	err = proto.Unmarshal(data, newTest)
+	if err != nil {
+		log.Fatal("unmarshaling error: ", err)
+	}
+	// Now test and newTest contain the same data.
+	if test.GetName() != newTest.GetName() {
+		log.Fatalf("data mismatch %q != %q", test.GetName(), newTest.GetName())
+	}
+	fmt.Println("test.GetName():", test.GetName() )
+	fmt.Println("newTest.GetName():", newTest.GetName() )
+}
